dc79197542260f60f9873c507df70a9266ef1a4f
Bartosz1301 <bglowacki.kcl@gmail.com>
2024-03-11 23:10:13 +0000

    initial commit

---
+++ crm/webapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Record(models.Model):
+    creation_date = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=255)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=300)
+    city = models.CharField(max_length=255)
+    province = models.CharField(max_length=200)
+    country = models.CharField(max_length=125)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return self.first_name + "   " + self.last_name
